61dc5abdb9a8ad3a362eb43df6a4a058023c452e
Gabriel Felipe <gabrielsaron16@gmail.com>
2023-03-01 15:33:41 -0300

    Create MaiorDeIdade.java

---
+++ aeronautica/eaoap/exercicios/MaiorDeIdade.java
@@ -0,0 +1,16 @@
+class MaiorDeIdade {
+
+    public static void main(String[] args) {
+
+        int idade = 63;
+
+        if(idade >= 60) {
+
+            System.out.println("É idoso(a)");
+
+        } else {
+
+            System.out.println("Não é idoso(a)");
+        }
+    }
+}
